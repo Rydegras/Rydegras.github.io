ec4af3e01034a145740069f26a72b76a120d2dbf
Ridegras <franciscominutti.fm@gmail.com>
2021-09-01 12:52:36 -0300

    Responsive Fixed Calculator

--- Calculator/app.js
+++ Calculator/app.js
@@ -21,9 +21,9 @@ const div = array=>{
 };
 
 const longNum = (number)=>{
-  if (number.toString().length > 16) {
+  if (number.toString().length > 20) {
     alert('number is too big!');
-    while(number.toString().length > 16){
+    while(number.toString().length > 20){
       number = number.toString().substring(0, number.length - 1);
     };
     cuenta = number;
